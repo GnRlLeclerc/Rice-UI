8dba9791959be3ab9c00aea5271f03ab5c629533
Thibaut <thibaut2saivre@gmail.com>
2025-08-07 23:27:45 +0200

    better tests for sizes

--- rice-layout/src/lib.rs
+++ rice-layout/src/lib.rs
@@ -20,173 +20,137 @@ pub use size::Size;
 mod tests {
     use crate::{AlignmentH, AlignmentV, Insets, arena::Arena, div::Div, size::Size};
 
+    /// Fixed sizes to test
+    static SIZES: &'static [i32] = &[0, 100, 200, 300];
+
     #[test]
-    fn test_div() {
-        let div = Div::default();
-        assert_eq!(div.height, Size::Fit);
-        assert_eq!(div.width, Size::Fit);
+    fn test_fixed_sizes() {
+        let mut arena = Arena::new();
+
+        for &width in SIZES {
+            for &height in SIZES {
+                let div = Div::new(Size::Fixed(width), Size::Fixed(height));
+                let key = arena.compute(&div);
+                let root = &arena.nodes[key];
+                assert_eq!(root.width, width);
+                assert_eq!(root.height, height);
+            }
+        }
     }
 
     #[test]
-    fn test_fixed_widths() {
+    fn test_fit_children() {
         let mut arena = Arena::new();
-        let div = Div::new(Size::Fixed(100), Size::Fit).children(vec![
-            Div::new(Size::Fixed(50), Size::Fixed(100)),
-            Div::new(Size::Fixed(100), Size::Fixed(200)),
-        ]);
 
-        let key = arena.compute(&div);
-        let root = &arena.nodes[key];
-        assert_eq!(root.width, 100);
-        let child1 = &arena.nodes[arena.children[key][0]];
-        assert_eq!(child1.width, 50);
-        let child2 = &arena.nodes[arena.children[key][1]];
-        assert_eq!(child2.width, 100);
-    }
-
-    #[test]
-    fn test_fit_widths_vertical() {
-        let mut arena = Arena::new();
-        let div = Div::new(Size::Fit, Size::Fit)
-            .children(vec![
-                Div::new(Size::Fixed(50), Size::Fixed(100)),
-                Div::new(Size::Fixed(100), Size::Fixed(200)),
-            ])
-            .vertical(AlignmentV::Left);
-
-        let key = arena.compute(&div);
-        let root = &arena.nodes[key];
-        assert_eq!(root.width, 100); // Should be the max of children widths
-        let child1 = &arena.nodes[arena.children[key][0]];
-        assert_eq!(child1.width, 50);
-        let child2 = &arena.nodes[arena.children[key][1]];
-        assert_eq!(child2.width, 100);
-    }
-
-    #[test]
-    fn test_fit_widths_horizontal() {
-        let mut arena = Arena::new();
-        let div = Div::new(Size::Fit, Size::Fit)
-            .children(vec![
-                Div::new(Size::Fixed(50), Size::Fixed(100)),
-                Div::new(Size::Fixed(100), Size::Fixed(200)),
-            ])
-            .horizontal(AlignmentH::Top);
-
-        let key = arena.compute(&div);
-        let root = &arena.nodes[key];
-        assert_eq!(root.width, 150); // Should be the sum of children widths
-        let child1 = &arena.nodes[arena.children[key][0]];
-        assert_eq!(child1.width, 50);
-        let child2 = &arena.nodes[arena.children[key][1]];
-        assert_eq!(child2.width, 100);
-    }
-
-    #[test]
-    fn test_fit_heights_vertical() {
-        let mut arena = Arena::new();
-        let div = Div::new(Size::Fit, Size::Fit)
-            .children(vec![
-                Div::new(Size::Fixed(50), Size::Fixed(100)),
-                Div::new(Size::Fixed(100), Size::Fixed(200)),
-            ])
-            .vertical(AlignmentV::Left);
-
-        let key = arena.compute(&div);
-        let root = &arena.nodes[key];
-        assert_eq!(root.height, 300); // Should be the sum of children heights
-        let child1 = &arena.nodes[arena.children[key][0]];
-        assert_eq!(child1.height, 100);
-        let child2 = &arena.nodes[arena.children[key][1]];
-        assert_eq!(child2.height, 200);
-    }
-
-    #[test]
-    fn test_fit_heights_horizontal() {
-        let mut arena = Arena::new();
-        let div = Div::new(Size::Fit, Size::Fit)
-            .children(vec![
-                Div::new(Size::Fixed(50), Size::Fixed(100)),
-                Div::new(Size::Fixed(100), Size::Fixed(200)),
-            ])
-            .horizontal(AlignmentH::Top);
-
-        let key = arena.compute(&div);
-        let root = &arena.nodes[key];
-        assert_eq!(root.height, 200); // Should be the max of children heights
-        let child1 = &arena.nodes[arena.children[key][0]];
-        assert_eq!(child1.height, 100);
-        let child2 = &arena.nodes[arena.children[key][1]];
-        assert_eq!(child2.height, 200);
+        for &width in SIZES {
+            for &height in SIZES {
+                let div = Div::new(Size::Fit, Size::Fit)
+                    .children(vec![Div::new(Size::Fixed(width), Size::Fixed(height))]);
+                let key = arena.compute(&div);
+                let root = &arena.nodes[key];
+                assert_eq!(root.width, width);
+                assert_eq!(root.height, height);
+            }
+        }
     }
 
     #[test]
     fn test_margins() {
         let mut arena = Arena::new();
-        let div = Div::new(Size::Fit, Size::Fit).children(vec![
-            Div::new(Size::Fixed(50), Size::Fixed(50)).margin(Insets {
-                left: 1,
-                top: 2,
-                right: 3,
-                bottom: 4,
-            }),
-        ]);
-
-        let key = arena.compute(&div);
-        let root = &arena.nodes[key];
-
-        assert_eq!(root.width, 54); // 50 + 1 + 3 = 54
-        assert_eq!(root.height, 56); // 50 + 2 + 4 = 56
+        for &margin_top in SIZES {
+            for &margin_right in SIZES {
+                for &margin_left in SIZES {
+                    for &margin_bottom in SIZES {
+                        let div = Div::new(Size::Fit, Size::Fit).children(vec![
+                            Div::new(Size::Fixed(100), Size::Fixed(100)).margin(Insets {
+                                left: margin_left,
+                                top: margin_top,
+                                right: margin_right,
+                                bottom: margin_bottom,
+                            }),
+                        ]);
+                        let key = arena.compute(&div);
+                        let root = &arena.nodes[key];
+                        assert_eq!(root.width, 100 + margin_left + margin_right);
+                        assert_eq!(root.height, 100 + margin_top + margin_bottom);
+                    }
+                }
+            }
+        }
     }
 
     #[test]
     fn test_padding() {
         let mut arena = Arena::new();
-        let div = Div::new(Size::Fit, Size::Fit)
-            .children(vec![Div::new(Size::Fixed(50), Size::Fixed(50))])
-            .padding(Insets {
-                left: 1,
-                top: 2,
-                right: 3,
-                bottom: 4,
-            });
-
-        let key = arena.compute(&div);
-        let root = &arena.nodes[key];
-
-        assert_eq!(root.width, 54); // 50 + 1 + 3 = 54
-        assert_eq!(root.height, 56); // 50 + 2 + 4 = 56
+        for &padding_top in SIZES {
+            for &padding_right in SIZES {
+                for &padding_left in SIZES {
+                    for &padding_bottom in SIZES {
+                        let div = Div::new(Size::Fit, Size::Fit)
+                            .children(vec![Div::new(Size::Fixed(100), Size::Fixed(100))])
+                            .padding(Insets {
+                                left: padding_left,
+                                top: padding_top,
+                                right: padding_right,
+                                bottom: padding_bottom,
+                            });
+                        let key = arena.compute(&div);
+                        let root = &arena.nodes[key];
+                        assert_eq!(root.width, 100 + padding_left + padding_right);
+                        assert_eq!(root.height, 100 + padding_top + padding_bottom);
+                    }
+                }
+            }
+        }
     }
 
     #[test]
-    fn test_align_vertical() {
+    fn test_fit_vertical() {
         let mut arena = Arena::new();
-        // Test centering without padding or margins
-        let div = Div::new(Size::Fixed(100), Size::Fixed(100))
-            .children(vec![Div::new(Size::Fixed(50), Size::Fixed(50))])
-            .vertical(AlignmentV::Center);
 
-        let key = arena.compute(&div);
-        let root = &arena.nodes[key];
-        assert_eq!(root.x, 0); // Parent position
+        for &width1 in SIZES {
+            for &width2 in SIZES {
+                for &height1 in SIZES {
+                    for &height2 in SIZES {
+                        let div = Div::new(Size::Fit, Size::Fit)
+                            .children(vec![
+                                Div::new(Size::Fixed(width1), Size::Fixed(height1)),
+                                Div::new(Size::Fixed(width2), Size::Fixed(height2)),
+                            ])
+                            .vertical(AlignmentV::Left);
 
-        let child = &arena.nodes[arena.children[key][0]];
-        assert_eq!(child.x, 25);
+                        let key = arena.compute(&div);
+                        let root = &arena.nodes[key];
+                        assert_eq!(root.width, width1.max(width2)); // Fit width should be max of children
+                        assert_eq!(root.height, height1 + height2); // Fit height should be sum of children
+                    }
+                }
+            }
+        }
     }
 
     #[test]
-    fn test_align_horizontal() {
+    fn test_fit_horizontal() {
         let mut arena = Arena::new();
-        // Test centering without padding or margins
-        let div = Div::new(Size::Fixed(100), Size::Fixed(100))
-            .children(vec![Div::new(Size::Fixed(50), Size::Fixed(50))])
-            .horizontal(AlignmentH::Center);
 
-        let key = arena.compute(&div);
-        let root = &arena.nodes[key];
-        assert_eq!(root.y, 0); // Parent position
+        for &width1 in SIZES {
+            for &width2 in SIZES {
+                for &height1 in SIZES {
+                    for &height2 in SIZES {
+                        let div = Div::new(Size::Fit, Size::Fit)
+                            .children(vec![
+                                Div::new(Size::Fixed(width1), Size::Fixed(height1)),
+                                Div::new(Size::Fixed(width2), Size::Fixed(height2)),
+                            ])
+                            .horizontal(AlignmentH::Top);
 
-        let child = &arena.nodes[arena.children[key][0]];
-        assert_eq!(child.y, 25);
+                        let key = arena.compute(&div);
+                        let root = &arena.nodes[key];
+                        assert_eq!(root.width, width1 + width2); // Fit width should be sum of children
+                        assert_eq!(root.height, height1.max(height2)); // Fit height should be max of children
+                    }
+                }
+            }
+        }
     }
 }
