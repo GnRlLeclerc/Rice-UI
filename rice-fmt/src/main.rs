1fd00a86857a0326906e7dc9af32bb07950b0be5
Thibaut <thibaut2saivre@gmail.com>
2025-08-24 19:50:03 +0200

    implement check & inplace formatter options

--- rice-fmt/src/main.rs
+++ rice-fmt/src/main.rs
@@ -1,10 +1,7 @@
-use std::{
-    fs,
-    io::{self},
-    path::Path,
-};
+use std::{fs, io, path::Path};
 
 use clap::Parser;
+use walkdir::WalkDir;
 
 use crate::root::format_source_file;
 
@@ -21,27 +18,72 @@ struct Args {
     /// File or folder to format
     #[arg(index = 1)]
     path: String,
+
+    /// Check formatting
+    #[arg(short, long, default_value_t = false)]
+    check: bool,
+
+    /// Format in place
+    #[arg(short, long, default_value_t = false)]
+    inplace: bool,
 }
 
 fn main() {
     let args = Args::parse();
+    let mut success = true;
 
     if Path::new(&args.path).is_dir() {
-        unimplemented!("format directory")
+        for entry in WalkDir::new(&args.path).into_iter().filter_map(|e| e.ok()) {
+            // Filter out non .rice files
+            match entry.file_name().to_str() {
+                Some(name) => {
+                    if !name.ends_with(".rice") {
+                        continue;
+                    }
+                }
+                None => continue,
+            }
+
+            success = success && format_file(entry.path(), args.inplace, args.check);
+        }
     } else {
-        let content = fs::read_to_string(&args.path)
-            .unwrap_or_else(|_| panic!("Failed to read file: {}", args.path));
+        success = format_file(&args.path, args.inplace, args.check);
+    }
 
-        let mut parser = tree_sitter::Parser::new();
-        parser
-            .set_language(&rice_grammar::LANGUAGE.into())
-            .expect("Error loading Rice grammar");
+    if args.check && !success {
+        eprintln!("Improper formatting for path {}", &args.path);
+        std::process::exit(1);
+    }
+}
 
-        let tree = parser
-            .parse(&content, None)
-            .expect("Error parsing content with Rice grammar");
+pub fn format_file<P: AsRef<Path>>(path: P, inplace: bool, check: bool) -> bool {
+    // Parse the file
+    let content = fs::read_to_string(&path)
+        .unwrap_or_else(|_| panic!("Failed to read file: {}", &path.as_ref().display()));
 
-        // Iterate through the tree and format it to the output
+    let mut parser = tree_sitter::Parser::new();
+    parser
+        .set_language(&rice_grammar::LANGUAGE.into())
+        .expect("Error loading Rice grammar");
+
+    let tree = parser
+        .parse(&content, None)
+        .expect("Error parsing content with Rice grammar");
+
+    // Format it with the given options
+    if inplace {
+        let mut file = fs::File::create(&path).unwrap();
+        format_source_file(tree, content.as_bytes(), &mut file).unwrap();
+    } else if check {
+        let mut buffer = Vec::new();
+        format_source_file(tree, content.as_bytes(), &mut buffer).unwrap();
+        let formatted = String::from_utf8(buffer).expect("Invalid UTF-8");
+        if formatted.cmp(&content) != std::cmp::Ordering::Equal {
+            return false;
+        }
+    } else {
         format_source_file(tree, content.as_bytes(), &mut io::stdout()).unwrap();
     }
+
+    true
 }
